b29ebcb45c1f71988c5c700cb821047913da0d2e
Jorge Coronado <60112119+JorgeACoronado@users.noreply.github.com>
2026-02-03 23:11:03 -0600

    Added card, changed name

--- Projects/week 3/even-tab-split/src/App.vue
+++ Projects/week 3/even-tab-split/src/App.vue
@@ -1,9 +1,70 @@
 <script setup>
+  import Header from './components/Header.vue'
+  import Card from './components/Card.vue'
 </script>
 
 <template>
-  <h1>Hello There!</h1>
+<Header/>
+<Card>
+  <main class="card">
+    <form id="personForm" class="rowForm">
+      <input id="personInput" type="text" placeholder="Add person name"/>
+      <button>Add Person</button>
+    </form>
+
+    <form id="expenseForm" class="rowForm">
+      <input id="descInput" type="text" placeholder="Expense Description"/>
+      <input id="amountInput" type="number" placeholder="Amount"/>
+      <select id="paidBySelect"></select>
+      <button>Add Expenses</button>
+    </form>
+
+    <section>
+      <h3>People</h3>
+      <ul id="peopleList" class="list"></ul>
+    </section>
+
+    <section>
+      <h3>Expenses</h3>
+      <ul id="expenseList" class="list"></ul>
+    </section>
+
+    <section>
+      <h3>Total</h3>
+      <p>
+        Total Spent: <strong id="totalSpent">$0.00</strong><br/>
+        Split Per person: <strong id="splitAmount">$0.00</strong>
+      </p>
+    </section>
+
+    <section class="summary">
+      <h3>Summary</h3>
+      <ul id="summaryList" class="list"></ul>
+    </section>
+  </main>
+</Card>
 </template>
 
 <style scoped>
+.rowForm {
+    Display: flex;
+    gap: 10px;
+    margin-bottom: 12px;
+}
+
+input, select {
+    flex: 1;
+    padding: 10px;
+    border-radius: 10px;
+    border: 1px solid #283618;
+}
+
+button {
+    padding: 10px 14px;
+    border-radius: 10px;
+    border: none;
+    background: #bc6c25;
+    color: #283618;
+    cursor: pointer;
+}
 </style>
